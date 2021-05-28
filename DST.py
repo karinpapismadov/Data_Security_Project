6e2d2ef61c3e227bc0206309ff23ae6217db7fa8
karin papismadov <57320551+karinpapismadov@users.noreply.github.com>
2021-05-28 19:03:34 +0300

    Create DST.py

---
+++ DST.py
@@ -0,0 +1,56 @@
+import math
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("DST: ")
+img = cv2.imread('strawberries.png', 0)
+img1 = img.astype('float')
+
+C_temp = np.zeros(img.shape)
+dst = np.zeros(img.shape)
+
+m, n = img.shape
+N = n
+for i in range(m):
+    for j in range(n):
+        C_temp[i, j] = np.sin(np.pi * (i + 1) * (j + 1) / (N + 1)) * np.sqrt(2 / (N + 1))
+
+dst = np.dot(C_temp, img1)
+dst = np.dot(dst, np.transpose(C_temp))
+dst1 = np.log(abs(dst))  # do log processing
+img_recor = np.dot(np.transpose(C_temp), dst)
+img_recor = np.dot(img_recor, C_temp)
+
+Y = np.square(np.subtract(img1,img_recor)).mean()
+print("MSE value of DST method:", Y)
+
+def PSNR(original, compressed):
+    mse = np.mean((original - compressed) ** 2)
+    if(mse == 0):  # MSE is zero means no noise is present in the signal .
+                  # Therefore PSNR have no importance.
+        return 100
+    max_pixel = 255.0
+    psnr = 20 * math.log10(max_pixel / math.sqrt(mse))
+    return psnr
+
+print("PSNR value of DST method: ",PSNR(img1, img_recor))
+
+
+plt.subplot(131)
+plt.imshow(img1, 'gray')
+plt.title('original image')
+plt.xticks([]), plt.yticks([])
+
+plt.subplot(132)
+plt.imshow(dst1)
+plt.title('DST')
+plt.xticks([]), plt.yticks([])
+
+plt.subplot(133)
+plt.imshow(img_recor, 'gray')
+plt.title('IDST')
+plt.xticks([]), plt.yticks([])
+
+plt.show()
+
